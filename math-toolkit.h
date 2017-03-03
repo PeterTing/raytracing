483891881a6f424118c696462014827b6aed05b8
PeterTing <a0917209079@gmail.com>
2017-03-03 23:41:14 +0800

    Make Makefile can use OpenMD and used loop-unrolling to the multiply_vector.

--- math-toolkit.h
+++ math-toolkit.h
@@ -1,12 +1,17 @@
 #ifndef __RAY_MATH_TOOLKIT_H
 #define __RAY_MATH_TOOLKIT_H
 
+#if defined(__GNUC__)
+#define FORCEDINLINE  __attribute__((always_FORCEDINLINE))
+#else
+#define FORCEDINLINE
+#endif
 
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
 
-static inline
+static FORCEDINLINE
 void normalize(double *v)
 {
     double d = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -17,45 +22,47 @@ void normalize(double *v)
     v[2] /= d;
 }
 
-static inline
+static FORCEDINLINE
 double length(const double *v)
 {
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-static inline
+static FORCEDINLINE
 void add_vector(const double *a, const double *b, double *out)
 {
-    //loop unrollinga
+
     out[0] = a[0] + b[0];
     out[1] = a[1] + b[1];
     out[2] = a[2] + b[2];
+
 }
 
-static inline
+static FORCEDINLINE
 void subtract_vector(const double *a, const double *b, double *out)
 {
-    //loop unrolling
     out[0] = a[0] - b[0];
     out[1] = a[1] - b[1];
     out[2] = a[2] - b[2];
 }
 
-static inline
+static FORCEDINLINE
 void multiply_vectors(const double *a, const double *b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] * b[i];
 }
 
-static inline
+static FORCEDINLINE
 void multiply_vector(const double *a, double b, double *out)
 {
-    for (int i = 0; i < 3; i++)
-        out[i] = a[i] * b;
+    //loop-unrolling
+    out[0] = a[0] * b;
+    out[1] = a[1] * b;
+    out[2] = a[2] * b;
 }
 
-static inline
+static FORCEDINLINE
 void cross_product(const double *v1, const double *v2, double *out)
 {
     out[0] = v1[1] * v2[2] - v1[2] * v2[1];
@@ -63,8 +70,8 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-//loop-unrolling
-static inline
+
+static FORCEDINLINE
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
@@ -73,7 +80,7 @@ double dot_product(const double *v1, const double *v2)
     return dp;
 }
 
-static inline
+static FORCEDINLINE
 void scalar_triple_product(const double *u, const double *v, const double *w,
                            double *out)
 {
@@ -81,7 +88,7 @@ void scalar_triple_product(const double *u, const double *v, const double *w,
     multiply_vectors(u, out, out);
 }
 
-static inline
+static FORCEDINLINE
 double scalar_triple(const double *u, const double *v, const double *w)
 {
     double tmp[3];
