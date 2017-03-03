6bbe03e6b1894b45bae89b58a363903e824f9849
PeterTing <a0917209079@gmail.com>
2017-03-03 17:59:37 +0800

    Use loop-unrolling skill and force inline to improve the performance. Loop-unrolling is used on 'dot_product'.

--- math-toolkit.h
+++ math-toolkit.h
@@ -1,11 +1,19 @@
 #ifndef __RAY_MATH_TOOLKIT_H
 #define __RAY_MATH_TOOLKIT_H
 
+//force inline
+#if defined(__GNUC__)
+#define FORCEDINLINE  __attribute__((always_inline))
+
+#else
+#define FORCEDINLINE
+#endif
+
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
 
-static inline
+static FORCEDINLINE
 void normalize(double *v)
 {
     double d = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -16,41 +24,41 @@ void normalize(double *v)
     v[2] /= d;
 }
 
-static inline
+static FORCEDINLINE
 double length(const double *v)
 {
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-static inline
+static FORCEDINLINE
 void add_vector(const double *a, const double *b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] + b[i];
 }
 
-static inline
+static FORCEDINLINE
 void subtract_vector(const double *a, const double *b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] - b[i];
 }
 
-static inline
+static FORCEDINLINE
 void multiply_vectors(const double *a, const double *b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] * b[i];
 }
 
-static inline
+static FORCEDINLINE
 void multiply_vector(const double *a, double b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] * b;
 }
 
-static inline
+static FORCEDINLINE
 void cross_product(const double *v1, const double *v2, double *out)
 {
     out[0] = v1[1] * v2[2] - v1[2] * v2[1];
@@ -58,16 +66,17 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-static inline
+//loop-unrolling
+static FORCEDINLINE
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
-    for (int i = 0; i < 3; i++)
-        dp += v1[i] * v2[i];
+
+    dp = v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
     return dp;
 }
 
-static inline
+static FORCEDINLINE
 void scalar_triple_product(const double *u, const double *v, const double *w,
                            double *out)
 {
@@ -75,7 +84,7 @@ void scalar_triple_product(const double *u, const double *v, const double *w,
     multiply_vectors(u, out, out);
 }
 
-static inline
+static FORCEDINLINE
 double scalar_triple(const double *u, const double *v, const double *w)
 {
     double tmp[3];
